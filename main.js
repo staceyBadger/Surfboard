37f50d86aa6d56ace5d5bee76e405a8ba2b9a42f
Anastasia Badger <jackrusselle007@gmail.com>
2021-08-29 21:49:12 +0500

    Сделано полноэкранное меню и слайдер

--- main.js
+++ main.js
@@ -9,3 +9,16 @@
      closebutton.addEventListener('click', function() {
       tabletsmenu.style.display = 'none';
   })
+
+  /*const options = document.querySelector('#shop-options');
+  const toggle = document.querySelector('#shop__link');
+
+  toggle.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (options.style.display == 'none') {
+        options.style.display = 'block';
+    } else {
+        options.style.display = 'none';
+    }
+    
+})*/
