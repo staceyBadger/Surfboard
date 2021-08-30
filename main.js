9f83d0526414d9ff1a7eaf6287d084eabeeee0bc
Anastasia Badger <jackrusselle007@gmail.com>
2021-08-30 18:38:11 +0500

    исправлены замечания

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
       const openbutton = document.querySelector('#hamburger');
       const tabletsmenu = document.querySelector('#menu');
       const closebutton = document.querySelector('#menu');
+      const closelink = document.querySelectorAll('.menu-tablets__link');
     
       openbutton.addEventListener('click', function() {
          tabletsmenu.style.display = 'flex';
-     })
+     });
      
      closebutton.addEventListener('click', function() {
       tabletsmenu.style.display = 'none';
-  })
+  });
 
-  /*const options = document.querySelector('#shop-options');
-  const toggle = document.querySelector('#shop__link');
-
-  toggle.addEventListener('click', function(event) {
+  /*closelink.addEventListener('click', function(event) {
     event.preventDefault();
-    if (options.style.display == 'none') {
-        options.style.display = 'block';
-    } else {
-        options.style.display = 'none';
-    }
-    
-})*/
+    tabletsmenu.style.display = 'none';
+  });*/
+
+  
+  closelink.forEach(function (entry) {
+  entry.addEventListener("click", function (event){
+    event.preventDefault();
+    tabletsmenu.style.display = 'none';
+  });
+});
+  
