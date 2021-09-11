f93bb35b0232b428a5826750184eec1b2355e826
Anastasia Badger <jackrusselle007@gmail.com>
2021-09-11 14:33:24 +0500

    Сделала фиксированное меню

--- ops.js
+++ ops.js
@@ -1,25 +1,54 @@
 const sections = $("section");
 const display = $(".maincontent");
 
+let inScroll = false;
+
 sections.first().addClass("active");
 
-const performTransition = sectionEq => {
-    const position = sectionEq * -100;
+const performTransition = (sectionEq) => {
+    if (inScroll === false) {
+        inScroll = true;
+        const position = sectionEq * -100;
 
-    display.css({
+        const currentSection = sections.eq(sectionEq);
+        const menuTheme = currentSection.attr("data-sidemenu-theme");
+        const sideMenu = $(".fixed-menu");
+
+
+        if(menuTheme === "black") {
+            sideMenu.addClass("fixed-menu-shadowed");
+        } else {
+            sideMenu.removeClass("fixed-menu-shadowed");
+        }
+
+        display.css({
         transform: `translateY(${position}%)`
     });
-}
+
+    sections.eq(sectionEq).addClass("active").siblings().removeClass("active");
+
+    
+
+    setTimeout(() => {
+        inScroll = false;
+
+        sideMenu.find(".fixed-menu__item").eq(sectionEq).addClass("fixed-menu__item--active").siblings().removeClass("fixed-menu__item--active");
+
+    }, 1000);
+  } 
+};
 
 const scrollViewport = direction => {
     const activeSection = sections.filter(".active");
+    const nextSection = activeSection.next();
+    const prevSection = activeSection.prev();
 
-    if (direction === "next") {
-
+    if (direction === "next" && nextSection.length) {
+        performTransition(nextSection.index())
     }
 
-    if (direction === "prev") {
-        
+    if (direction === "prev" && prevSection.length) {
+        performTransition(prevSection.index())        
     }
 }
 
@@ -27,12 +56,42 @@ $(window).on("wheel", e => {
     const deltaY = e.originalEvent.deltaY;
 
     if (deltaY > 0) {
-        performTransition(2);
-        //next
+        
+        scrollViewport("next");
     }
 
     if (deltaY < 0) {
-        //prev
+        scrollViewport("prev");
+        
     }
     
+});
+
+$(window).on("keydown", (e) => {
+
+    const tagName = e.target.tagName.toLowerCase();
+
+    if (tagName !== "input" && tagName !== "textarea") {
+
+        switch(e.keyCode) {
+            case 38: //prev
+                scrollViewport("prev");
+                break;
+    
+            case 40: //next
+                scrollViewport("next");
+                break;
+    
+        }
+    }
+});
+
+$("[data-scroll-to]").click(e => {
+    e.preventDefault();
+
+    const $this = $(e.currentTarget);
+    const target = $this.attr("data-scroll-to");
+    const reqSection = $(`[data-section-id=${target}]`);
+
+    performTransition(reqSection.index());
 });
